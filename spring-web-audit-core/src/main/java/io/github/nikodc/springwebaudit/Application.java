8b859706923dc20eb7ef9c02371c797a1aea015b
nikodc <nikodc2@gmail.com>
2016-08-01 00:49:00 -0300

    -- Agrego anotation @AuditableMethod como marcador de operaciones a las que desea publicar valores de parámetros y retorno.  - Agrego construcciones sobre AOP para extraer valores de parámetros y retorno de los @AuditableMethod's.

--- spring-web-audit-core/src/main/java/io/github/nikodc/springwebaudit/Application.java
+++ spring-web-audit-core/src/main/java/io/github/nikodc/springwebaudit/Application.java
@@ -2,18 +2,12 @@ package io.github.nikodc.springwebaudit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public AuditInfoListener auditInfoListener() {
-        return new StreamAuditInfoListener(System.out);
     }
 
 }
